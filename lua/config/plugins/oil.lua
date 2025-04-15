298b6d77dc1b1c1eac6307bc5b34f6405d4d0dd9
Davi Cheli Miquelim <davichelimiquelim@gmail.com>
2025-04-15 09:49:13 -0300

    feat: add oil config

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -5,7 +5,20 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     lazy = false,
     config = function()
+      local oil = require("oil")
+      oil.setup({
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<C-l>"] = false,
+          ["<C-k>"] = false,
+          ["<C-j>"] = false,
+          ["<M-H>"] = "actions.select_split",
+        },
+        view_options = { show_hidden = true },
+      })
+
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end
+    end,
   }
 }
