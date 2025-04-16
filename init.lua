662d7d578597aa360cc4f3b03c64501aca688cf5
Davi Cheli Miquelim <davichelimiquelim@gmail.com>
2025-04-16 08:17:24 -0300

    feat: change formatting to only work on save

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
 vim.opt.number = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.relativenumber = true
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
@@ -12,16 +12,16 @@ vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.keymap.set("n", "<space>st", function()
-    vim.cmd.vnew()
-    vim.cmd.term()
-    vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 15)
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
 end)
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -20,7 +20,12 @@ return {
     opts = {
       servers = {
         lua_ls = {},
-        ts_ls = {}
+        ts_ls = {
+          on_attach = function(client, bufnr)
+            client.server_capabalities.documentFormattingProvider = false
+            client.server_capabalities.documentRangeFromattingProvider = false
+          end
+        }
       }
     },
     config = function(_, opts)
@@ -44,12 +49,7 @@ return {
 
           if client:supports_method('textDocument/formatting') then
             -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end,
-            })
+            vim.keymap.set("n", "<space>ff", function() vim.lsp.buf.format({ bufnr = args.buf, id = client.id }) end)
           end
         end,
       })
